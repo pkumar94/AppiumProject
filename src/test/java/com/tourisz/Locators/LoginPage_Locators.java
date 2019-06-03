64d9fc332a2515982fb39b4c2fcea18a8f3e9911
Pravinkumar Sarkate <PS11184490@softenger.local>
2019-06-03 10:50:49 +0530

    some changs done

--- src/test/java/com/tourisz/Locators/LoginPage_Locators.java
+++ src/test/java/com/tourisz/Locators/LoginPage_Locators.java
@@ -5,7 +5,7 @@ public class LoginPage_Locators
 	//Login Page Locators 
 	public static String username_TextField="com.softenger.sofq:id/etUserId";
 	public static String password_TextField="com.softenger.sofq:id/etPassword";
-	public static String login_Button="//android.widget.Button[@text='LOG IN']";
+	public static String login_Button="//android.widget.Button[@text='LOG']";
 	//public static String errorMessage= "//span[contains(text(),'Authentication Failed')]";
 	
 	
